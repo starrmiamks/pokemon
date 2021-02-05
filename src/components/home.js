66d272a9047e9a55882040f38870ce4bfb48eaba
Mia Starr <starrmiamks@gmail.com>
2021-02-04 20:40:57 -0500

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import Card from './card';
+
+const Home = () => {
+    const [data, setData] = useState();
+    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+
+    const initData = async () => {
+        const response = await fetch(url);
+        const pokemon = await response.json();
+
+        setData(pokemon);
+    };
+
+    useEffect(() => {
+        initData();
+    }, [url]);
+
+    return (
+        <div>
+            {data?.previous && (<button onClick={() => setUrl(data.previous)}>Previous</button>)}
+            {data?.next && (<button onClick={() => setUrl(data.next)}>Next</button>)}
+            {data?.results.map((pokemon, index) => (
+                <Card name={pokemon.name} key={index} />
+            ))}
+        </div>
+    );
+};
+
+export default Home;
